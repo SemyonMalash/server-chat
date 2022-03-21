fd0d5f3a07a81b26a6086ae57f15ab5d340669ed
SemyonMalash <floan732@gmail.com>
2022-03-21 20:18:03 +0300

    Practice

--- src/main/java/ru/itsjava/Application.java
+++ src/main/java/ru/itsjava/Application.java
@@ -1,27 +1,11 @@
 package ru.itsjava;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.ServerSocket;
-import java.net.Socket;
+import ru.itsjava.services.ServerServiceImpl;
 
 public class Application {
-    public final static int PORT = 8081;
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(PORT);
-
-        System.out.println("Server is being started");
-
-        while (true) {
-            Socket socket = serverSocket.accept();
-            if (socket != null) {
-                System.out.println("Client is connected");
-                BufferedReader bufferedReader =
-                        new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                System.out.println(bufferedReader.readLine());
-            }
-        }
+    public static void main(String[] args) {
+        ServerServiceImpl serverService = new ServerServiceImpl();
+        serverService.start();
     }
 }
